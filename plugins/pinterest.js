8cde3cd8c870b442880b3a26ad4108f77a5e34b2
Ilman <87686497+ilmanhdyt@users.noreply.github.com>
2021-12-16 15:47:41 +0800

    update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -9,7 +9,7 @@ Contoh *${usedPrefix}${command} Minecraft|1*`
       jumlah = 1
       m.reply('Untuk jumlah yang lebih banyak,  anda harus menjadi member *premium!!*')
   } 
-  let res = await fetch(global.API('bg', '/pins', { q: query }))
+  let res = await fetch(global.API('zekz', '/api/pinimg', { q: query }))
   let json = await res.json()
   if (json.status !== true) throw json
   for (let i = 0; i < jumlah; i++) {
@@ -24,7 +24,7 @@ Contoh *${usedPrefix}${command} Minecraft|1*`
 }
 handler.help = ['pinterest'].map(v => v + ' [teks]')
 handler.tags = ['internet', 'downloader']
-handler.command = /^(pinterest)$/i
+handler.command = /^(pinterest|pint)$/i
 
 module.exports = handler
 
